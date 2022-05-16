93de84ef01357f3a83ec285b88b274ae5049ea79
1Door <2753652621@qq.com>
2022-05-16 23:23:05 +0800

    find the bug in 9.8.1.shader.gs

--- HelloExplode.cpp
+++ HelloExplode.cpp
@@ -30,210 +30,8 @@
  float deltaTime = 0.0f;
  float lastFrame = 0.0f;
 
-// //lighting
-// glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
-
-// int main()
-// {
-//     // glfw: initialize and configure
-//     // ------------------------------
-//     glfwInit();
-//     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-//     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-//     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-// #ifdef __APPLE__
-//     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-// #endif
-
-//     // glfw window creation
-//     // --------------------
-//     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Art is explosion", NULL, NULL);
-//     if (window == NULL)
-//     {
-//         std::cout << "Failed to create GLFW window" << std::endl;
-//         glfwTerminate();
-//         return -1;
-//     }
-//     glfwMakeContextCurrent(window);
-//     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-//     glfwSetCursorPosCallback(window, mouse_callback);
-//     glfwSetScrollCallback(window, scroll_callback);
-
-//     // tell GLFW to capture our mouse
-//     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-
-//     // glad: load all OpenGL function pointers
-//     // ---------------------------------------
-//     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-//     {
-//         std::cout << "Failed to initialize GLAD" << std::endl;
-//         return -1;
-//     }
-
-//     // tell stb_image.h to flip loaded texture's on the y-axis (before loading model).
-//     // stbi_set_flip_vertically_on_load(true);
-
-//     // configure global opengl state
-//     // -----------------------------
-//     glEnable(GL_DEPTH_TEST);
-
-//     // build and compile shaders
-//     // -------------------------
-//     Shader ourShader("./Shaders/9.8.1.shader.vs",
-//         "./Shaders/9.8.1.shader.fs",
-//         "./Shaders/9.8.1.shader.gs");
-
-//     // load models
-//     // -----------
-//     Model ourModel("./Model/nanosuit/nanosuit.obj");
-//     //Model ourModel("./Model/ModelUsedforGraphics/ModelforGraphics.fbx");
-//     //Model ourModel("./Model/Haku/TDA Lacy Haku.pmx");
-
-
-
-
-//     // draw in wireframe
-//     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
-//     // render loop
-//     // -----------
-//     while (!glfwWindowShouldClose(window))
-//     {
-//         // per-frame time logic
-//         // --------------------
-//         float currentFrame = static_cast<float>(glfwGetTime());
-//         deltaTime = currentFrame - lastFrame;
-//         lastFrame = currentFrame;
-
-//         // input
-//         // -----
-//         processInput(window);
-
-//         // render
-//         // ------
-//         glClearColor(0.2f, 0.3f, 0.5f, 1.0f);
-//         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-
-
-
-//         glDrawArrays(GL_TRIANGLES, 0, 36);
-//         // don't forget to enable shader before setting uniforms
-//         ourShader.use();
-
-//         // // view/projection transformations
-//         // glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-//         // glm::mat4 view = camera.GetViewMatrix();
-//         // ourShader.setMat4("projection", projection);
-//         // ourShader.setMat4("view", view);
-
-//         // // render the loaded model
-//         // glm::mat4 model = glm::mat4(1.0f);
-//         // model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f)); // translate it down so it's at the center of the scene
-//         // model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));	// it's a bit too big for our scene, so scale it down
-//         // ourShader.setMat4("model", model);
-
-//         // ourShader.setFloat("time",static_cast<float>(glfwGetTime()));
-
-//         // ourModel.Draw(ourShader);
-
-
-
-
-//         // // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-//         // // -------------------------------------------------------------------------------
-//         // glfwSwapBuffers(window);
-//         // glfwPollEvents();
-//         // configure transformation matrices
-//         glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 1.0f, 100.0f);
-//         glm::mat4 view = camera.GetViewMatrix();;
-//         glm::mat4 model = glm::mat4(1.0f);
-//         ourShader.use();
-//         ourShader.setMat4("projection", projection);
-//         ourShader.setMat4("view", view);
-//         ourShader.setMat4("model", model);
-
-//         // add time component to geometry shader in the form of a uniform
-//         ourShader.setFloat("time", static_cast<float>(glfwGetTime()));
-
-//         // draw model
-//         ourModel.Draw(ourShader);
-
-//         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-//         // -------------------------------------------------------------------------------
-//         glfwSwapBuffers(window);
-//         glfwPollEvents();
-//     }
-
-
-//     // glfw: terminate, clearing all previously allocated GLFW resources.
-//     // ------------------------------------------------------------------
-//     glfwTerminate();
-//     return 0;
-// }
-
-// // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// // ---------------------------------------------------------------------------------------------------------
-// void processInput(GLFWwindow* window)
-// {
-//     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-//         glfwSetWindowShouldClose(window, true);
-
-//     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-//         camera.ProcessKeyboard(FORWARD, deltaTime);
-//     if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-//         camera.ProcessKeyboard(BACKWARD, deltaTime);
-//     if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-//         camera.ProcessKeyboard(LEFT, deltaTime);
-//     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-//         camera.ProcessKeyboard(RIGHT, deltaTime);
-//     if (glfwGetKey(window, GLFW_KEY_L) == GLFW_PRESS)
-//         glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-//     if (glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS)
-//         glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-// }
-
-// // glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// // ---------------------------------------------------------------------------------------------
-// void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-// {
-//     // make sure the viewport matches the new window dimensions; note that width and 
-//     // height will be significantly larger than specified on retina displays.
-//     glViewport(0, 0, width, height);
-// }
-
-// // glfw: whenever the mouse moves, this callback is called
-// // -------------------------------------------------------
-// void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
-// {
-//     float xpos = static_cast<float>(xposIn);
-//     float ypos = static_cast<float>(yposIn);
-
-//     if (firstMouse)
-//     {
-//         lastX = xpos;
-//         lastY = ypos;
-//         firstMouse = false;
-//     }
-
-//     float xoffset = xpos - lastX;
-//     float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
-
-//     lastX = xpos;
-//     lastY = ypos;
-
-//     camera.ProcessMouseMovement(xoffset, yoffset);
-// }
-
-// // glfw: whenever the mouse scroll wheel scrolls, this callback is called
-// // ----------------------------------------------------------------------
-// void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
-// {
-//     camera.ProcessMouseScroll(static_cast<float>(yoffset));
-// }
-
-
+//lighting
+glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
 
 int main()
 {
@@ -250,7 +48,7 @@ int main()
 
     // glfw window creation
     // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Art is explosion", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -273,19 +71,30 @@ int main()
         return -1;
     }
 
+    // tell stb_image.h to flip loaded texture's on the y-axis (before loading model).
+    // stbi_set_flip_vertically_on_load(true);
+
     // configure global opengl state
     // -----------------------------
     glEnable(GL_DEPTH_TEST);
 
     // build and compile shaders
     // -------------------------
-    Shader shader("./Shaders/9.8.1.shader.vs",
-                "./Shaders/9.8.1.shader.fs",
-                 "./Shaders/9.8.1.shader.gs");
+    Shader ourShader("./Shaders/9.8.1.shader.vs",
+        "./Shaders/9.8.1.shader.fs",
+        "./Shaders/9.8.1.shader.gs");
 
     // load models
     // -----------
-    Model nanosuit("./Model/nanosuit/nanosuit.obj");
+    //Model ourModel("./Model/nanosuit/nanosuit.obj");
+    //Model ourModel("./Model/ModelUsedforGraphics/ModelforGraphics.fbx");
+    Model ourModel("./Model/Haku/TDA Lacy Haku.pmx");
+
+
+
+
+    // draw in wireframe
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     // render loop
     // -----------
@@ -303,23 +112,53 @@ int main()
 
         // render
         // ------
-        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClearColor(0.2f, 0.3f, 0.5f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+
+
+
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        // don't forget to enable shader before setting uniforms
+        ourShader.use();
+
+        // // view/projection transformations
+        // glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        // glm::mat4 view = camera.GetViewMatrix();
+        // ourShader.setMat4("projection", projection);
+        // ourShader.setMat4("view", view);
+
+        // // render the loaded model
+        // glm::mat4 model = glm::mat4(1.0f);
+        // model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f)); // translate it down so it's at the center of the scene
+        // model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));	// it's a bit too big for our scene, so scale it down
+        // ourShader.setMat4("model", model);
+
+        // ourShader.setFloat("time",static_cast<float>(glfwGetTime()));
+
+        // ourModel.Draw(ourShader);
+
+
+
+
+        // // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // // -------------------------------------------------------------------------------
+        // glfwSwapBuffers(window);
+        // glfwPollEvents();
         // configure transformation matrices
         glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 1.0f, 100.0f);
         glm::mat4 view = camera.GetViewMatrix();;
         glm::mat4 model = glm::mat4(1.0f);
-        shader.use();
-        shader.setMat4("projection", projection);
-        shader.setMat4("view", view);
-        shader.setMat4("model", model);
+        ourShader.use();
+        ourShader.setMat4("projection", projection);
+        ourShader.setMat4("view", view);
+        ourShader.setMat4("model", model);
 
         // add time component to geometry shader in the form of a uniform
-        shader.setFloat("time", static_cast<float>(glfwGetTime()));
+        ourShader.setFloat("time", static_cast<float>(glfwGetTime()));
 
         // draw model
-        nanosuit.Draw(shader);
+        ourModel.Draw(ourShader);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
@@ -327,6 +166,9 @@ int main()
         glfwPollEvents();
     }
 
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
     glfwTerminate();
     return 0;
 }
@@ -346,6 +188,10 @@ void processInput(GLFWwindow* window)
         camera.ProcessKeyboard(LEFT, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
         camera.ProcessKeyboard(RIGHT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_L) == GLFW_PRESS)
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    if (glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS)
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
@@ -363,6 +209,7 @@ void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
 {
     float xpos = static_cast<float>(xposIn);
     float ypos = static_cast<float>(yposIn);
+
     if (firstMouse)
     {
         lastX = xpos;
@@ -385,3 +232,6 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
     camera.ProcessMouseScroll(static_cast<float>(yoffset));
 }
+
+
+
