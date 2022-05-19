5770fa5fffdb82b9f602c890b3723b398034ce46
1Door <2753652621@qq.com>
2022-05-19 19:00:01 +0800

    bug

---
+++ HelloInstancing.cpp
@@ -0,0 +1,429 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "shader_s.h"
+#include "camera.h"
+#include "model.h"
+
+#include<iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+// void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+// void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+// void processInput(GLFWwindow* window);
+
+// // settings
+// const unsigned int SCR_WIDTH = 800;
+// const unsigned int SCR_HEIGHT = 600;
+
+// // camera
+// Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+// float lastX = SCR_WIDTH / 2.0f;
+// float lastY = SCR_HEIGHT / 2.0f;
+// bool firstMouse = true;
+
+// // timing
+// float deltaTime = 0.0f;
+// float lastFrame = 0.0f;
+
+// //lighting
+// glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+
+// int main()
+// {
+//     // glfw: initialize and configure
+//     // ------------------------------
+//     glfwInit();
+//     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+//     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+//     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+// #ifdef __APPLE__
+//     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+// #endif
+
+//     // glfw window creation
+//     // --------------------
+//     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "HelloModel", NULL, NULL);
+//     if (window == NULL)
+//     {
+//         std::cout << "Failed to create GLFW window" << std::endl;
+//         glfwTerminate();
+//         return -1;
+//     }
+//     glfwMakeContextCurrent(window);
+//     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+//     glfwSetCursorPosCallback(window, mouse_callback);
+//     glfwSetScrollCallback(window, scroll_callback);
+
+//     // tell GLFW to capture our mouse
+//     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+//     // glad: load all OpenGL function pointers
+//     // ---------------------------------------
+//     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+//     {
+//         std::cout << "Failed to initialize GLAD" << std::endl;
+//         return -1;
+//     }
+
+//     // tell stb_image.h to flip loaded texture's on the y-axis (before loading model).
+//     // stbi_set_flip_vertically_on_load(true);
+
+//     // configure global opengl state
+//     // -----------------------------
+//     glEnable(GL_DEPTH_TEST);
+
+//     // build and compile shaders
+//     // -------------------------
+//     Shader ourShader("./Shaders/9.9.1.shader.vs",
+//         "./Shaders/9.9.1.shader.fs");
+
+//     // Shader normalShader("./Shaders/9.8.2.shader.vs",
+//     //     "./Shaders/9.8.2.shader.fs",
+//     //     "./Shaders/9.8.2.shader.gs");
+
+//     // load models
+//     // -----------
+//     //Model ourModel("./Model/nanosuit/nanosuit.obj");
+//     //Model ourModel("./Model/ModelUsedforGraphics/ModelforGraphics.fbx");
+//     //Model ourModel("./Model/Haku/TDA Lacy Haku.pmx");
+    
+//     glm::vec2 translations[100];
+//     int index = 0;
+//     float offset = 0.1f;
+//     for (int y=-10;y<10;y +=2)
+//     {
+//         for(int x = -10;x<10;x+=2)
+//         {
+//             glm::vec2 translation;
+//             translation.x = (float)x/10.0f + offset;
+//             translations[index++] = translation;
+//         }
+//     }
+
+//     unsigned int instanceVBO;
+//     glGenBuffers(1,&instanceVBO);
+//     glBindBuffer(GL_ARRAY_BUFFER,instanceVBO);
+//     glBufferData(GL_ARRAY_BUFFER,sizeof(glm::vec2)* 100,&translations[0],GL_STATIC_DRAW);
+//     glBindBuffer(GL_ARRAY_BUFFER,0);
+    
+//     float quadVertices[]={
+//         // positions     // colors
+//         -0.05f,  0.05f,  1.0f, 0.0f, 0.0f,
+//          0.05f, -0.05f,  0.0f, 1.0f, 0.0f,
+//         -0.05f, -0.05f,  0.0f, 0.0f, 1.0f,
+
+//         -0.05f,  0.05f,  1.0f, 0.0f, 0.0f,
+//          0.05f, -0.05f,  0.0f, 1.0f, 0.0f,
+//          0.05f,  0.05f,  0.0f, 1.0f, 1.0f
+//     };
+
+//     unsigned int quadVAO,quadVBO;
+//     glGenVertexArrays(1,&quadVAO);
+//     glGenBuffers(1,&quadVBO);
+//     glBindVertexArray(quadVAO);
+//     glBindBuffer(GL_ARRAY_BUFFER,quadVBO);
+//     glBufferData(GL_ARRAY_BUFFER,sizeof(quadVertices),quadVertices,GL_STATIC_DRAW);
+//     glEnableVertexAttribArray(0);
+//     glVertexAttribPointer(0,2,GL_FLOAT,GL_FALSE,5 * sizeof(float),(void*)0);
+//     glEnableVertexAttribArray(1);
+//     glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,5 * sizeof(float),(void*)(2 * sizeof(float)));
+    
+//     //set insrance data
+//     glEnableVertexAttribArray(2);
+//     glBindBuffer(GL_ARRAY_BUFFER,instanceVBO);
+//     glVertexAttribPointer(2,2,GL_FLOAT,GL_FALSE,2 * sizeof(float),(void*)0);
+//     glBindBuffer(GL_ARRAY_BUFFER,0);
+//     glVertexAttribDivisor(2,1);// we need to tell OpenGL this is an instanced vertex attribute.
+
+
+    
+
+//     // draw in wireframe
+//     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+//     // render loop
+//     // -----------
+//     while (!glfwWindowShouldClose(window))
+//     {
+//         // per-frame time logic
+//         // --------------------
+//         float currentFrame = static_cast<float>(glfwGetTime());
+//         deltaTime = currentFrame - lastFrame;
+//         lastFrame = currentFrame;
+
+//         // input
+//         // -----
+//         processInput(window);
+
+//         // render
+//         // ------
+//         // glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
+//         // glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+
+
+//         // glDrawArrays(GL_TRIANGLES, 0, 36);
+//         // // don't forget to enable shader before setting uniforms
+//         // ourShader.use();
+
+//         // // view/projection transformations
+//         // glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+//         // glm::mat4 view = camera.GetViewMatrix();
+//         // ourShader.setMat4("projection", projection);
+//         // ourShader.setMat4("view", view);
+
+//         // // render the loaded model
+//         // glm::mat4 model = glm::mat4(1.0f);
+//         // model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f)); // translate it down so it's at the center of the scene
+//         // model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));	// it's a bit too big for our scene, so scale it down
+//         // ourShader.setMat4("model", model);
+//         // ourModel.Draw(ourShader);
+
+//         // normalShader.use();
+//         // normalShader.setMat4("projection", projection);
+//         // normalShader.setMat4("view", view);
+//         // normalShader.setMat4("model", model);
+
+//         // ourModel.Draw(normalShader);
+
+//         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+//         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+//         // draw 100 instanced quads
+//         ourShader.use();
+//         glBindVertexArray(quadVAO);
+//         glDrawArraysInstanced(GL_TRIANGLES, 0, 6, 100); // 100 triangles of 6 vertices each
+//         glBindVertexArray(0);
+
+
+
+
+
+//         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+//         // -------------------------------------------------------------------------------
+//         glfwSwapBuffers(window);
+//         glfwPollEvents();
+//     }
+//     glDeleteVertexArrays(1,&quadVAO);
+//     glDeleteBuffers(1,&quadVBO);
+
+
+//     // glfw: terminate, clearing all previously allocated GLFW resources.
+//     // ------------------------------------------------------------------
+//     glfwTerminate();
+//     return 0;
+// }
+
+// // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// // ---------------------------------------------------------------------------------------------------------
+// void processInput(GLFWwindow* window)
+// {
+//     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+//         glfwSetWindowShouldClose(window, true);
+
+//     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+//         camera.ProcessKeyboard(FORWARD, deltaTime);
+//     if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+//         camera.ProcessKeyboard(BACKWARD, deltaTime);
+//     if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+//         camera.ProcessKeyboard(LEFT, deltaTime);
+//     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+//         camera.ProcessKeyboard(RIGHT, deltaTime);
+//     if (glfwGetKey(window, GLFW_KEY_L) == GLFW_PRESS)
+//         glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+//     if (glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS)
+//         glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+// }
+
+// // glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// // ---------------------------------------------------------------------------------------------
+// void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+// {
+//     // make sure the viewport matches the new window dimensions; note that width and 
+//     // height will be significantly larger than specified on retina displays.
+//     glViewport(0, 0, width, height);
+// }
+
+// // glfw: whenever the mouse moves, this callback is called
+// // -------------------------------------------------------
+// void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+// {
+//     float xpos = static_cast<float>(xposIn);
+//     float ypos = static_cast<float>(yposIn);
+
+//     if (firstMouse)
+//     {
+//         lastX = xpos;
+//         lastY = ypos;
+//         firstMouse = false;
+//     }
+
+//     float xoffset = xpos - lastX;
+//     float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+//     lastX = xpos;
+//     lastY = ypos;
+
+//     camera.ProcessMouseMovement(xoffset, yoffset);
+// }
+
+// // glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// // ----------------------------------------------------------------------
+// void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+// {
+//     camera.ProcessMouseScroll(static_cast<float>(yoffset));
+// }
+
+
+
+
+
+
+
+
+
+
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+int main()
+{
+    // glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // glfw window creation
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // configure global opengl state
+    // -----------------------------
+    glEnable(GL_DEPTH_TEST);
+
+    // build and compile shaders
+    // -------------------------
+    Shader shader("./Shaders/9.9.1.shadervs", "./Shaders/9.9.1.shader.fs");
+
+    // generate a list of 100 quad locations/translation-vectors
+    // ---------------------------------------------------------
+    glm::vec2 translations[100];
+    int index = 0;
+    float offset = 0.1f;
+    for (int y = -10; y < 10; y += 2)
+    {
+        for (int x = -10; x < 10; x += 2)
+        {
+            glm::vec2 translation;
+            translation.x = (float)x / 10.0f + offset;
+            translation.y = (float)y / 10.0f + offset;
+            translations[index++] = translation;
+        }
+    }
+
+    // store instance data in an array buffer
+    // --------------------------------------
+    unsigned int instanceVBO;
+    glGenBuffers(1, &instanceVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec2) * 100, &translations[0], GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    float quadVertices[] = {
+        // positions     // colors
+        -0.05f,  0.05f,  1.0f, 0.0f, 0.0f,
+         0.05f, -0.05f,  0.0f, 1.0f, 0.0f,
+        -0.05f, -0.05f,  0.0f, 0.0f, 1.0f,
+
+        -0.05f,  0.05f,  1.0f, 0.0f, 0.0f,
+         0.05f, -0.05f,  0.0f, 1.0f, 0.0f,
+         0.05f,  0.05f,  0.0f, 1.0f, 1.0f
+    };
+    unsigned int quadVAO, quadVBO;
+    glGenVertexArrays(1, &quadVAO);
+    glGenBuffers(1, &quadVBO);
+    glBindVertexArray(quadVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(2 * sizeof(float)));
+    // also set instance data
+    glEnableVertexAttribArray(2);
+    glBindBuffer(GL_ARRAY_BUFFER, instanceVBO); // this attribute comes from a different vertex buffer
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glVertexAttribDivisor(2, 1); // tell OpenGL this is an instanced vertex attribute.
+
+
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+        // render
+        // ------
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // draw 100 instanced quads
+        shader.use();
+        glBindVertexArray(quadVAO);
+        glDrawArraysInstanced(GL_TRIANGLES, 0, 6, 100); // 100 triangles of 6 vertices each
+        glBindVertexArray(0);
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    glDeleteVertexArrays(1, &quadVAO);
+    glDeleteBuffers(1, &quadVBO);
+
+    glfwTerminate();
+    return 0;
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
